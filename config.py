cf9286c1866569966676e55ee79a8be65790789a
alirezavahdaniasl83-svg <alirezavahdaniasl83@gmail.com>
2026-08-07 06:25:24 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "0"))
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "0"))
+
+    API_NAME = "NajatDehande"
+    API_VERSION = "0.2.0"
+
+config = Config()
